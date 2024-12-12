54433304ee41006d9a4b335bd47eadca4d7180ee
suyogshejal2004 <161924832+suyogshejal2004@users.noreply.github.com>
2024-12-12 23:27:40 +0530

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -5,52 +5,60 @@ import Link from "next/link";
 const AboutDetails = () => {
   return (
     <section className="py-20 w-full">
-      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full">
         <ItemLayout
           className={
-            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+            "col-span-full lg:col-span-8 row-span-2 flex-col items-start"
           }
         >
-          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
             Architect of Enchantment
           </h2>
-          <p className="font-light  text-xs sm:text-sm md:text-base   ">
-            My journey in web development is powered by an array of mystical
-            tools and languages, with JavaScript casting the core of my
-            enchantments. I wield frameworks like React.js and Next.js with
-            precision, crafting seamless portals (websites) that connect realms
-            (users) across the digital universe. The ancient arts of the
-            Jamstack empower me to create fast, secure, and dynamic experiences,
-            while my design skills ensure every creation is not only functional
-            but visually captivating. Join me as I continue to explore new
-            spells and technologies to shape the future of the web.
+          <p className="font-light text-xs sm:text-sm md:text-base">
+            Hi! I'm Suyog Shejal, a passionate and driven Software Developer
+            currently pursuing my BCA at Amrita Vishwa Vidyapeetham. I am eager
+            to build and contribute to innovative projects in Android
+            development, web development, and problem-solving.
+            <br />
+            Although I'm at the beginning of my career, I'm constantly learning
+            and growing. I have a strong foundation in Kotlin, Jetpack Compose,
+            ReactJS, and JavaScript, and I'm always excited to explore new
+            technologies. I am dedicated to improving my skills and gaining
+            hands-on experience by collaborating on meaningful projects.
+            <br />
+            I am enthusiastic about taking on challenges that allow me to grow,
+            learn, and push my limits. Whether it's coding, problem-solving, or
+            building applications, I strive to provide quality work and
+            contribute to projects that make a positive impact.
+            <br />
+            Feel free to reach out if you're interested in collaborating or
+            have any questions.
           </p>
         </ItemLayout>
 
         <ItemLayout
-          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-pink-500"}
         >
-          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            25+ <sub className="font-semibold text-base">clients</sub>
+          <p className="font-semibold w-full text-left text-3xl sm:text-6xl">
+            Eager to Work with Clients{" "}
+            <sub className="font-semibold text-lg sm:text-xl">and Grow</sub>
           </p>
         </ItemLayout>
 
         <ItemLayout
-          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-pink-500"}
         >
-          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            4+{" "}
-            <sub className="font-semibold text-base">years of experience</sub>
+          <p className="font-semibold w-full text-left text-3xl sm:text-6xl">
+            Eager to Learn{" "}
+            <sub className="font-semibold text-lg sm:text-xl">and Grow</sub>
           </p>
         </ItemLayout>
 
-        <ItemLayout
-          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
-        >
+        <ItemLayout className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-            alt="CodeBucks"
+            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=suyogshejal2004&size_weight=0.5&count_weight=0.5&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="Top Languages"
             loading="lazy"
           />
         </ItemLayout>
@@ -58,17 +66,17 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-            alt="CodeBucks"
+            src={`https://github-readme-stats.vercel.app/api?username=suyogshejal2004&show_icons=true&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            alt="GitHub Stats"
             loading="lazy"
           />
         </ItemLayout>
 
         <ItemLayout className={"col-span-full"}>
-          <img
+        <img
             className="w-full h-auto"
-            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
-            alt="CodeBucks"
+            src={`https://skillicons.dev/icons?i=kotlin,eclipse,react,tailwind,js,html,css,github,git,vscode,androidstudio,idea,java,mysql,py,replit,sqlite,windows`}
+            alt="Skills"
             loading="lazy"
           />
         </ItemLayout>
@@ -76,22 +84,22 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
-            alt="CodeBucks"
+            src={`https://streak-stats.demolab.com?user=suyogshejal2004&theme=transparent&hide_border=true&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            alt="GitHub Streak"
             loading="lazy"
           />
         </ItemLayout>
 
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <Link
-            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
+            href="https://github.com/suyogshejal2004"
             target="_blank"
             className="w-full"
           >
-            <img
+           <img
               className="w-full h-auto"
-              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
-              alt="CodeBucks"
+              src="https://github-readme-stats.vercel.app/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B"
+              alt="Pinned Repo"
               loading="lazy"
             />
           </Link>
